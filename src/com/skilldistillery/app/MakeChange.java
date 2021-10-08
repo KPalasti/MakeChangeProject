c63a1ef4810ed211738398a8d65a4baa5a2d5577
Kyle Palasti <Kyle.Palasti@gmail.com>
2021-10-08 16:20:06 -0600

    Created Menu Method

--- src/com/skilldistillery/app/MakeChange.java
+++ src/com/skilldistillery/app/MakeChange.java
@@ -1,9 +1,16 @@
 package com.skilldistillery.app;
 
+import java.util.Scanner;
+
 public class MakeChange {
 
 	public static void main(String[] args) {
 
+		Scanner scanner = new Scanner(System.in);
+	}
+	
+	public void menu() {
+		//Method here
 	}
 
 }
